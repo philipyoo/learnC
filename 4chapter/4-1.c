0cf68e943f9ed51f981f110093a649e4c25304cd
Philip Yoo <philipyoo10@gmail.com>
2016-09-07 17:00:18 -0700

    Create ch4 dir

---
+++ 4chapter/4-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+/*
+  Write the function `strrindex(s,t)` which returns the position of the
+  rightmost occurrence of t in s, or -1 if there is none.
+*/
+
+int main(void) {
+  
+}
